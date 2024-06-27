3b8c3f4a6653b1842d4abff9382aa6699bd9c24f
nhi <nhi.tran220915@gmail.edu.vn>
2024-06-27 13:39:19 +0700

    upd

---
+++ data/books.js
@@ -0,0 +1,143 @@
+[
+    {
+        "name": "When Breath Becomes Air",
+        "price": "170000",
+        "stock": 37,
+        "description": "When Breath Becomes Air is a poignant memoir by Dr. Paul Kalanithi, who faced terminal lung cancer. The book delves into his profound reflections on life, death, and the meaning of existence.",
+        "categories": ["Non-fiction"],
+        "src": "./resource/b-0.jpg"
+    },
+    {
+        "name": "And The Mountains Echoed",
+        "price": "135000",
+        "stock": 58,
+        "description": "And The Mountains Echoed is a novel by Khaled Hosseini that explores the bonds between siblings and how choices impact their lives over decades.",
+        "categories": ["Fiction"],
+        "src": "./resource/BOOK-2.jpg"
+    },
+    {
+        "name": "Master Of His Fate: The House Of Falconer",
+        "price": "150000",
+        "stock": 74,
+        "description": "Master Of His Fate: The House Of Falconer is a historical fiction novel by Barbara Taylor Bradford, set in Victorian England and follows the story of James Falconer.",
+        "categories": ["Fiction"],
+        "src": "./resource/BOOK-3.jpg"
+    },
+    {
+        "name": "Anne Of Green Gables (A Bantam Classic)",
+        "price": "120000",
+        "stock": 22,
+        "description": "Anne Of Green Gables is a classic children's novel by L.M. Montgomery, telling the story of an imaginative and talkative orphan named Anne Shirley.",
+        "categories": ["Children"],
+        "src": "./resource/BOOK-4.jpg"
+    },
+    {
+        "name": "Sharpe's Assassin",
+        "price": "90000",
+        "stock": 19,
+        "description": "Sharpe's Assassin is a historical fiction novel by Bernard Cornwell, featuring the adventures of Richard Sharpe during the Napoleonic Wars.",
+        "categories": ["Fiction"],
+        "src": "./resource/BOOK-5.jpg"
+    },
+    {
+        "name": "The Kite Runner",
+        "price": "170000",
+        "stock": 45,
+        "description": "The Kite Runner is a novel by Khaled Hosseini that tells the story of Amir, a young boy from a wealthy family in Kabul, and his journey of redemption.",
+        "categories": ["Fiction"],
+        "src": "./resource/BOOK-6.jpg"
+    },
+    {
+        "name": "Andersen's Fairy Tales",
+        "price": "125000",
+        "stock": 89,
+        "description": "Andersen's Fairy Tales is a collection of stories by Hans Christian Andersen, including famous tales like The Little Mermaid and The Ugly Duckling.",
+        "categories": ["Children"],
+        "src": "./resource/BOOK-7.jpg"
+    },
+    {
+        "name": "The Book Thief",
+        "price": "200000",
+        "stock": 10,
+        "description": "The Book Thief is a novel by Markus Zusak set in Nazi Germany.",
+        "categories": ["Fiction"],
+        "src": "./resource/BOOK-8.jpg"
+    },
+    {
+        "name": "Ivy Newt And The Time Thief",
+        "price": "155000",
+        "stock": 26,
+        "description": "",
+        "categories": ["Fiction"],
+        "src": "./resource/BOOK-9.jpg"
+    },{
+        "name": "The Little Book Of Aliens",
+        "price": "75000",
+        "stock": 10,
+        "description": "",
+        "categories": ["Non-fiction"],
+        "src": "./resource/BOOK-10.jpg"
+    },{
+        "name": "The Thief Who Sang Storms",
+        "price": "210000",
+        "stock": 75,
+        "description": "",
+        "categories": ["Children"],
+        "src": "./resource/BOOK-11.jpg"
+    },{
+        "name": "The Graveyard Book",
+        "price": "250000",
+        "stock": 43,
+        "description": "",
+        "categories": ["Fiction"],
+        "src": "./resource/BOOK-12.jpg"
+    },{
+        "name": "Sapiens: A Brief History of Humankind",
+        "price": "150000",
+        "stock": 27,
+        "description": "",
+        "categories": ["Children"],
+        "src": "./resource/BOOK-13.jpg"
+      },
+      {
+        "name": "Think and Grow Rich",
+        "price": "195000",
+        "stock": 35,
+        "description": "",
+        "categories": ["Children"],
+        "src": "./resource/BOOK-14.jpg"
+      },
+      {
+        "name": "The Thing You Can See Only When You Slow Down",
+        "price": "270000",
+        "stock": 19,
+        "description": "",
+        "categories": ["Fiction"],
+        "src": "./resource/BOOK-15.jpg"
+      },
+      {
+        "name": "Charlotte's Web",
+        "price": "190000",
+        "stock": 22,
+        "description": "",
+        "categories": ["Children"],
+        "src": "./resource/BOOK-16.jpg"
+      },
+      {
+        "name": "To Kill A Mockingbird",
+        "price": "135000",
+        "stock": 14,
+        "description": "",
+        "categories": ["Fiction"],
+        "src": "./resource/BOOK-17.jpg"
+      },
+      {
+        "name": "Wonder",
+        "price": "165000",
+        "stock": 30,
+        "description": "",
+        "categories": ["Children"],
+        "src": "./resource/BOOK-18.jpg"
+      }
+]
+
